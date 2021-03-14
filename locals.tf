8e8a0e890884d94e8965c4b9d6b923ed1a1452c5
Dylan Turnbull <dylanturn@gmail.com>
2021-03-14 10:03:35 -0500

    make sure the local is used

--- locals.tf
+++ locals.tf
@@ -1,12 +1,12 @@
 locals {
   deployment_volumes = merge(var.empty_dir_volumes, var.secret_volumes, var.config_map_volumes)
-  containers = defaults(var.containers[0], {
-    image_tag                       = "latest"
-    image_pull_policy               = "IfNotPresent"
-    is_init                         = false
-    cpu_request                     = "25m"
-    memory_request                  = "32Mi"
-    cpu_limit                       = "50m"
-    memory_limit                    = "64Mi"
-  })
+#  containers = defaults(var.containers[0], {
+#    image_tag                       = "latest"
+#    image_pull_policy               = "IfNotPresent"
+#    is_init                         = false
+#    cpu_request                     = "25m"
+#    memory_request                  = "32Mi"
+#    cpu_limit                       = "50m"
+#    memory_limit                    = "64Mi"
+#  })
 }
