5e0d5255fa6a10649386b7e73e44fbb0bee0c914
Dylan Turnbull <dylanturn@gmail.com>
2021-03-13 18:40:21 -0600

    fixed default values

--- locals.tf
+++ locals.tf
@@ -1,18 +1,18 @@
 locals {
   deployment_volumes = merge(var.empty_dir_volumes, var.secret_volumes, var.config_map_volumes)
   containers = defaults(var.containers, {
-    image_tag         = "latest"
-    image_pull_policy = "IfNotPresent"
-    is_init           = false
-    cpu_request       = "25m"
-    memory_request    = "32Mi"
-    cpu_limit         = "50m"
-    memory_limit      = "64Mi"
-    ports             = []
-    simple_environment_variables = {}
-    secret_environment_variables = {}
+    image_tag                       = "latest"
+    image_pull_policy               = "IfNotPresent"
+    is_init                         = false
+    cpu_request                     = "25m"
+    memory_request                  = "32Mi"
+    cpu_limit                       = "50m"
+    memory_limit                    = "64Mi"
+    simple_environment_variables    = {}
+    secret_environment_variables    = {}
     configmap_environment_variables = {}
-    http_get_liveness_probe = []
-    http_get_readiness_probe = []
+    ports                           = []
+    http_get_liveness_probe         = []
+    http_get_readiness_probe        = []
   })
 }
